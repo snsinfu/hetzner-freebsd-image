ccb0ea4e6523886bd2192322a88a5fd532b0b4a9
snsinfu <snsinfu@gmail.com>
2020-03-10 01:52:44 +0900

    Add terraform configuration for freebsd image creation

---
+++ assets/firstboot.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -u
+
+# Grow root filesystem.
+rootpart=$(mount -p | awk '$2 == "/" { print $1 }')
+rootdisk=${rootpart%p*}
+rootpartno=${rootpart##*p}
+gpart recover ${rootdisk}
+gpart resize -i ${rootpartno} ${rootdisk}
+growfs -y ${rootpart}
+
+# Set hostname.
+sysrc hostname="$(fetch -o- http://169.254.169.254/latest/meta-data/hostname)"
+service hostname restart
+
+# Host key
+rm -f /etc/ssh/ssh_host_*
+ssh-keygen -A
+pgrep sshd.pid || service sshd restart
+
+# User script
+fetch -o- http://169.254.169.254/latest/user-data | sh
