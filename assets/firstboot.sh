63ce5764cc757786ae5e52ccb6799cd40ad2ba1a
snsinfu <snsinfu@gmail.com>
2020-03-12 02:45:21 +0900

    Replace growfs & ssh-keygen with rc's built-in ones
    FreeBSD's rc already provides growfs service [1]. It runs at the
    firstboot time (i.e., when /firstboot sentinel exists). Seemingly the
    sentinel file is not created by the installer,so we create one in the
    imprinting script.
    
    Also, the sshd service automatically generates host keys when not exist
    [2]. It has a benefit of the generated keys being configurable via
    rc.conf variables like sshd_rsa_enable="NO".
    
    [1]: https://github.com/freebsd/freebsd/blob/release/12.1.0/libexec/rc/rc.d/growfs
    [2]: https://github.com/freebsd/freebsd/blob/release/12.1.0/libexec/rc/rc.d/sshd

--- assets/firstboot.sh
+++ assets/firstboot.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 set -u
 
-# Grow root filesystem.
-rootpart=$(mount -p | awk '$2 == "/" { print $1 }')
-rootdisk=${rootpart%p*}
-rootpartno=${rootpart##*p}
-gpart recover ${rootdisk}
-gpart resize -i ${rootpartno} ${rootdisk}
-growfs -y ${rootpart}
-
 # Set hostname.
 sysrc hostname="$(fetch -o- http://169.254.169.254/latest/meta-data/hostname)"
 service hostname restart
-
-# Host key
-rm -f /etc/ssh/ssh_host_*
-ssh-keygen -A
-pgrep sshd && service sshd restart
 
 # User script
 fetch -o- http://169.254.169.254/latest/user-data | sh
